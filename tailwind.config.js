0e2a8ec993c1082a5edf72742f30d1e870d71f89
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-01-24 01:23:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/helper/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      xxs: "320px",
+      xs: "360px",
+
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+};
