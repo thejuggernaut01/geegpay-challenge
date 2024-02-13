5a11eaa1c9bcea63ba54709f97daff347c91c352
Ayoola Adewale <111049444+thejuggernaut01@users.noreply.github.com>
2024-02-13 12:08:30 +0100

    Update MainBar.jsx

--- src/components/MainBar.jsx
+++ src/components/MainBar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Barchart } from "./BarChart";
 import {
   GreenChartIcon,
